377edf986fbab1a3872a1d12221ac52ee7c1d8d7
mustafaiman <mustafa.iman@hazelcast.com>
2015-11-13 16:13:21 +0200

    seems completed

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include<limits.h>
+#include<time.h>
 
 #include "utils.h"
 
@@ -20,11 +21,6 @@ void kreduce(int *kleast, int *myids, int *myvals, int k, int world_size, int my
     MPI_Gather(myvals, k, MPI_INT, recvValues, k, MPI_INT, 0, MPI_COMM_WORLD);
     
     if(myid == 0) {
-        printf("Results accumulated\n");
-        for (int i=0; i< world_size * k; i++) {
-            printf("%d %d\n", recvReferences[i], recvValues[i]);
-        }
-        
         int *indexes = (int *)malloc(sizeof(int) * world_size);
         memset(indexes, 0, sizeof(int) * world_size);
         
@@ -52,6 +48,10 @@ int main(int argc, char *argv[]) {
 	3 => documents.txt
 	4 => query.txt
 	*/
+    double timeStartSerial;
+    double timeEndSerial;
+    double timeStartParallel;
+    double timeEndParallel;
 	int MY_ID, NUM_PROCS;
 	MPI_Comm_size(MPI_COMM_WORLD, &NUM_PROCS);
 	MPI_Comm_rank(MPI_COMM_WORLD, &MY_ID);
@@ -65,6 +65,7 @@ int main(int argc, char *argv[]) {
 	int *partitionFileIds;
 
 	if(MY_ID == 0) {
+        timeStartSerial = MPI_Wtime();
 		int *fileIds;
 		int *content;
 		int *query;
@@ -75,28 +76,9 @@ int main(int argc, char *argv[]) {
 		readFromFile(argv[3], &fileIds, &content, dictionarySize, &numberOfFiles);
 		readQueryFromFile(argv[4], &query, dictionarySize);
 
-		for(i = 0; i<numberOfFiles; i++) {
-			printf("%d : ", fileIds[i]);
-			for (j = 0; j < dictionarySize; j++)
-			{
-				printf("%d ", content[i*dictionarySize + j]);
-			}
-			printf("\n");
-		}
-
-		for(i = 0; i<dictionarySize; i++) {
-			printf("%d ", query[i]);
-		}
-		printf("\n");
-
 		int *similarities = (int *)malloc(sizeof(int) * numberOfFiles);
 
 		calculateSimilarities(content, query, similarities, numberOfFiles, dictionarySize);
-
-		printf("Similarities\n");
-		for(i=0; i<numberOfFiles; i++) {
-			printf("%d %d\n", fileIds[i], similarities[i]);
-		}
 
 		partitionSize = numberOfFiles / NUM_PROCS + (numberOfFiles % NUM_PROCS == 0 ? 0 : 1);
 		int offset = partitionSize;
@@ -113,7 +95,13 @@ int main(int argc, char *argv[]) {
 			MPI_Send(similarities + offset, temporaryPartitionCount, MPI_INT, i, 0xACE5, MPI_COMM_WORLD);
 			MPI_Send(fileIds + offset, temporaryPartitionCount, MPI_INT, i, 0xACE5, MPI_COMM_WORLD);
 			offset += temporaryPartitionCount;
-		}
+        }
+        
+        timeEndSerial = MPI_Wtime();
+        printf("Sequential Part: %f\n", timeEndSerial - timeStartSerial);
+        timeStartParallel = MPI_Wtime();
+        
+        
         for(i=numberOfFiles % NUM_PROCS; i< NUM_PROCS;i++) {
             if( i == 0)
                 continue;
@@ -123,7 +111,6 @@ int main(int argc, char *argv[]) {
 			MPI_Send(fileIds + offset, temporaryPartitionCount, MPI_INT, i, 0xACE5, MPI_COMM_WORLD);
 			offset += temporaryPartitionCount;
 		}
-
 	} else {
 
 		MPI_Recv(&partitionSize, 1, MPI_INT, 0, 0xACE5, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -136,16 +123,7 @@ int main(int argc, char *argv[]) {
 
 	}
 	int i;
-    /*
-	for(i=0; i<partitionSize; i++) {
-		printf("process-similarities%d %d\n",MY_ID, partitionSimilarities[i]);
-	}
-	for(i=0; i<partitionSize; i++) {
-		printf("process-fileIds%d %d\n",MY_ID, partitionFileIds[i]);
-	}
-	printf("\n");
-    */
-    printf("Sorting\n");
+
     modifiedQuickSort(partitionFileIds, partitionSimilarities, 0, partitionSize-1);
     
     //If k is larger than partitionSize, this is neccessary to prevent garbage cluttering minimum selection in kreduce
@@ -153,18 +131,20 @@ int main(int argc, char *argv[]) {
         partitionSimilarities[i] = INT_MAX;
     }
     
-    for(i=0; i<partitionSize; i++) {
-        printf("p%d partiiont-size=%d %d %d\n",MY_ID, partitionSize, partitionSimilarities[i], partitionFileIds[i]);
-    }
     int *kleast;
     if( MY_ID == 0) {
         kleast = (int *)malloc(sizeof(int) * kvalue);
     }
     
     kreduce(kleast, partitionFileIds, partitionSimilarities, kvalue, NUM_PROCS, MY_ID);
+
     
-    if(MY_ID == 0 ) {
-        printf("Results\n");
+    
+    if(MY_ID == 0) {
+        timeEndParallel = MPI_Wtime();
+        printf("Paralel Part: %f\n", timeEndParallel - timeStartParallel);
+        printf("Total Time: %f\n", timeEndParallel - timeStartSerial);
+        printf("Least k = %d ids\n", kvalue);
         for (int i=0; i<kvalue; i++) {
             printf("%d\n", kleast[i]);
         }
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-mpirun -n 11 ./a.out 3 5 documents.txt query.txt
+mpirun -n 2 ./a.out 3 5 documents.txt query.txt
